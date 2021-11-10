f851825ce1bf63a461dd046ae50942542a915c66
Aqu <sheenix15@gmail.com>
2021-11-10 16:29:27 +0800

    released the first private beta of the mod.

--- src/main/java/io/github/navalarmamentsrebooted/config/NavalArmamentsRebootedConfig.java
+++ src/main/java/io/github/navalarmamentsrebooted/config/NavalArmamentsRebootedConfig.java
@@ -13,27 +13,36 @@ public class NavalArmamentsRebootedConfig {
         public final ConfigValue<Integer> goldenVaulDamage;
         public final ConfigValue<Integer> diamondVaulDamage;
         public final ConfigValue<Integer> netheriteVaulDamage;
+        //Chaos Awakens Integration
         public final ConfigValue<Integer> emeraldVaulDamage;
         public final ConfigValue<Integer> tigersEyeVaulDamage;
         public final ConfigValue<Integer> amethystVaulDamage;
         public final ConfigValue<Integer> rubyVaulDamage;
         public final ConfigValue<Integer> ultimateVaulDamage;
+        //Weapon Skill Stats
+        public final ConfigValue<Integer> vaulSkillDamage;
+        public final ConfigValue<Double> vaulSkillRange;
 
         Common(ForgeConfigSpec.Builder builder) {
             builder.push("Attack Damage");
             builder.push("Vaul");
-            woodenVaulDamage = builder.define("Damage of the Wooden Vaul", 8);
-            stoneVaulDamage = builder.define("Damage of the Stone Vaul", 10);
-            ironVaulDamage = builder.define("Damage of the Iron Vaul", 11);
-            goldenVaulDamage = builder.define("Damage of the Golden Vaul", 10);
-            diamondVaulDamage = builder.define("Damage of the Diamond Vaul", 12);
-            netheriteVaulDamage = builder.define("Damage of the Netherite Vaul", 13);
+            woodenVaulDamage = builder.define("Damage of the Wooden Vaul", 9);
+            stoneVaulDamage = builder.define("Damage of the Stone Vaul", 12);
+            ironVaulDamage = builder.define("Damage of the Iron Vaul", 13);
+            goldenVaulDamage = builder.define("Damage of the Golden Vaul", 9);
+            diamondVaulDamage = builder.define("Damage of the Diamond Vaul", 14);
+            netheriteVaulDamage = builder.define("Damage of the Netherite Vaul", 16);
             //Chaos Awakens Integration
-            emeraldVaulDamage = builder.define("Damage of the Emerald Vaul", 14);
-            tigersEyeVaulDamage = builder.define("Damage of the Tiger's Eye Vaul", 12);
-            amethystVaulDamage = builder.define("Damage of the Amethyst Vaul", 22);
+            emeraldVaulDamage = builder.define("Damage of the Emerald Vaul", 18);
+            tigersEyeVaulDamage = builder.define("Damage of the Tiger's Eye Vaul", 20);
+            amethystVaulDamage = builder.define("Damage of the Amethyst Vaul", 23);
             rubyVaulDamage = builder.define("Damage of the Amethyst Vaul", 28);
-            ultimateVaulDamage = builder.define("Damage of the Ultimate Vaul", 45);
+            ultimateVaulDamage = builder.define("Damage of the Ultimate Vaul (Usually added by +1)", 51);
+
+            //Weapon Skill Stats
+            builder.push("Weapon Skill Stats");
+            vaulSkillDamage = builder.define("Default damage of the Vauls' skill", 3);
+            vaulSkillRange = builder.define("Default range of the Vauls' skill (measured in blocks) (must add a 'd' at the end of the number to function properly.)", 7d);
             builder.pop();
         }
     }
--- src/main/java/io/github/navalarmamentsrebooted/items/VaulItem.java
+++ src/main/java/io/github/navalarmamentsrebooted/items/VaulItem.java
@@ -1,22 +1,38 @@
 package io.github.navalarmamentsrebooted.items;
 
+import io.github.navalarmamentsrebooted.config.NavalArmamentsRebootedConfig;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.*;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class VaulItem extends AxeItem {
     protected EffectInstance tippedEffect;
-    protected EffectInstance tippedEffect2;
 
     public VaulItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, Item.Properties builder) {
         super(tier, attackDamageIn, attackSpeedIn, builder);
     }
 
-    public VaulItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, EffectInstance effect, EffectInstance effect2, Item.Properties builder) {
+    public VaulItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, EffectInstance effect, Item.Properties builder) {
         super(tier, attackDamageIn, attackSpeedIn, builder);
         this.tippedEffect = effect;
-        this.tippedEffect2 = effect2;
     }
 
     @Override
@@ -24,10 +40,40 @@ public class VaulItem extends AxeItem {
         boolean retval = super.hurtEnemy(stack, entity, source);
         if (this.tippedEffect != null) {
             entity.addEffect(tippedEffect);
-            entity.addEffect(tippedEffect2);
+        }
+        if ((EntityTypeTags.getAllTags().getTagOrEmpty(new ResourceLocation(("forge:vaul_targets").toLowerCase(java.util.Locale.ENGLISH)))
+                .contains(entity.getType()))) {
+            entity.hurt(DamageSource.GENERIC, getAttackDamage() + 5.0F);
         }
 
         entity.setDeltaMovement(0, 0.5, 0);
         return retval;
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity entity, Hand hand) {
+        ActionResult<ItemStack> actionResult = super.use(world, entity, hand);
+        double x = entity.getX();
+        double y = entity.getY();
+        double z = entity.getZ();
+        List<Entity> _targets = world.getEntitiesOfClass(Entity.class, new AxisAlignedBB(entity.getX() - (NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get()  / 2d), entity.getY() - ((double) NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get() / 2d), entity.getZ() - ((double) NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get() / 2d), entity.getX() + ((double) NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get() / 2d), entity.getY() + ((double) NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get() / 2d), entity.getZ() + ((double) NavalArmamentsRebootedConfig.COMMON.vaulSkillRange.get() / 2d)), null)
+                .stream().sorted(new Object() {
+                    Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                        return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.distanceToSqr(_x, _y, _z)));
+                    }
+                }.compareDistOf(entity.getX(), entity.getY(), entity.getZ())).collect(Collectors.toList());
+        for (Entity targettedEntities : _targets) {
+            if ((!(entity == targettedEntities))) {
+                targettedEntities.setDeltaMovement(0, 0.5, 0);
+                targettedEntities.hurt(DamageSource.MAGIC, (float) NavalArmamentsRebootedConfig.COMMON.vaulSkillDamage.get());
+                /*((ServerWorld) world).sendParticles(ParticleTypes.POOF, (targettedEntities.getX()), (targettedEntities.getY()),
+                        (targettedEntities.getZ()), (int) 20, 0.2, 0.2, 0.2, 0.1);*/
+            }
+        }
+        ((PlayerEntity) entity).getCooldowns().addCooldown((this.getItem()).getItem(), (int) 200);
+        ((ClientWorld) world).playLocalSound(x, y, z,
+                (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.land")),
+                SoundCategory.NEUTRAL, (float) 1, (float) 1, false);
+        return actionResult;
     }
 }
